ea0e1d854a2575b90cf7f5121b3845c39b08948e
vladimirbessonov <2000.besson@gmail.com>
2020-04-26 14:39:01 -0700

    try on heroku

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api')
 require('dotenv').config()
-const Koa = require('koa')
-const Router = require('koa-router')
-const bodyParser = require('koa-bodyparser')
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TOKEN
+const token = process.env.TELEGRAM_TOKEN
 const port = process.env.PORT
 const url = process.env.url  || 'https://colivline.herokuapp.com:443';
 
 const options = {
     webHook: {
-        // Port to which you should bind is assigned to $PORT variable
-        // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
         port: port
-        // you do NOT need to set up certificates since Heroku provides
-        // the SSL certs already (https://<app-name>.herokuapp.com)
-        // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
     }
 };
 
@@ -25,21 +16,6 @@ const bot = new TelegramBot(token, options);
 bot.openWebHook()
 bot.setWebHook(`${url}/bot${token}`)
 
-//
-// const app = new Koa()
-// const router = Router()
-// router.post('/bot', ctx => {
-//     const { body} = ctx.request;
-//     bot.processUpdate(body)
-//     ctx.status = 200
-// })
-// app.use(bodyParser())
-// app.use(router.routes())
-// app.listen(process.env.PORT, ()=> {
-//     console.log(`Server runs on port ${process.env.PORT}`)
-
-// })
-// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
